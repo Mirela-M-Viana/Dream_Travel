0b51f38f7862db55c274ddfa3d09fcc090b0f84b
Mirela Viana <vianamirela.melo@gmail.com>
2025-08-20 22:14:21 -0300

    Validação de quantidade de passagens e cálculo de valor total implementados
    -Criação da página agendamento.js.
    - Adicionada validação para impedir quantidades inválidas (menores que 1 ou não numéricas).
    - Exibição de mensagem de erro com fundo vermelho caso a entrada seja inválida.
    - Inclusão do cálculo do valor total utilizando a função `calcularTotal`.
    - Correção na exibição dos valores de hotel e passagem no formato monetário brasileiro.
    - "l" em "qtd" é um erro proposital.

---
+++ js/agendamentos.js
@@ -0,0 +1,45 @@
+//Código para calcular uma linha
+            // let mult_qtd_passagem = document.querySelector(".qtd").textContent * document.querySelector(".valor-passagem").textContent
+            // let valorHotel = parseInt(document.querySelector(".valor-hotel").textContent)
+            // document.querySelector(".valor-total").textContent = mult_qtd_passagem + valorHotel
+
+            //Captura todas as encomendas para calcular o total
+            var clientes = document.querySelectorAll(".cliente");
+
+    for (var count = 0; count < clientes.length; count++) {
+        // Captura o cliente atual
+        var clientesGeral = clientes[count];
+
+        // Captura a quantidade de passagens
+        var qtdPassagem = parseInt(clientesGeral.querySelector(".qtd").textContent);
+
+        // Captura o valor da passagem
+        var valorPassagem = parseInt(clientesGeral.querySelector(".valor-passagem").textContent);
+
+        // Captura o valor do hotel
+        var valorHotel = parseInt(clientesGeral.querySelector(".valor-hotel").textContent);
+
+
+
+        //Validação
+        if(qtdPassagem < 1 || isNaN(qtdPassagem)){
+            clientesGeral.querySelector(".qtd").textContent = "Quantidade inválida"
+            clientesGeral.querySelector(".qtd").style.backgroundColor= "red";
+        }else {
+            //calcula o valor total
+            clientesGeral.querySelector(".valor-total").textContent = calcularTotal(qtdPassagem,valorPassagem,valorHotel);
+
+            clientesGeral.querySelector(".valor-hotel", ".valor-passagem").textContent = parseFloat(valorHotel, valorPassagem).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        }
+
+
+    }
+
+    //FUNÇÃO
+function calcularTotal(qtdPassagem_agendamento, valorPassagem_agendamento, valorHotel_agendamento){
+        var total = 0
+
+        total = (qtdPassagem_agendamento * valorPassagem_agendamento) + valorHotel_agendamento;
+
+        return total.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})
+}
